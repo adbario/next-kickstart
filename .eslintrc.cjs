d1987c683a87a16b9bb720bb5534863c77164b6e
Riku Särkinen <riku@adbar.io>
2023-07-30 03:13:10 +0100

    Use stricter type checking

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint", "prettier", "tailwindcss"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  rules: {
+    "tailwindcss/no-custom-classname": "off",
+    "prettier/prettier": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
+
+module.exports = config;
